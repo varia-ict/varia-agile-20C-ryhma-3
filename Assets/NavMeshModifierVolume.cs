9e9f0e5a85253658aef7d3d420f4a1ed2e1799f7
Niamat2222 <niamat.akbari@outlook.com>
2021-10-29 15:11:07 +0300

    EnemyC

---
+++ Assets/NavMeshModifierVolume.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityEngine.AI
+{
+    internal class NavMeshModifierVolume
+    {
+        internal static List<NavMeshModifierVolume> activeModifiers;
+        internal bool isActiveAndEnabled;
+        internal object transform;
+
+        internal bool AffectsAgentType(int m_AgentTypeID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
