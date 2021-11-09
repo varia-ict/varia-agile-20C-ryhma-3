cee1bc45f17eeb467bdedafcc892df34396d8dbb
Niamat2222 <niamat.akbari@outlook.com>
2021-11-09 13:50:30 +0200

    enemyXo

---
+++ Assets/GameNeeds/enemyPumpkin/New Folder/script/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform tomato;
+    public float distans;
+    public float speed;
+    public float howClose;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tomato = GameObject.FindGameObjectWithTag("Tomato").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distans = Vector3.Distance(tomato.position, transform.position);
+
+        if (distans <= howClose)
+        {
+            transform.LookAt(tomato);
+            GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+        }
+
+        if (distans <= 1.5f)
+        {
+
+        }
+    }
+}
