9d977ea22938d60faa90f9b851171909f24252fb
Niamat2222 <niamat.akbari@outlook.com>
2021-11-18 14:50:31 +0200

    enemypickup

--- Assets/GameNeeds/enemyPumpkin/New Folder/script/Enemy.cs
+++ Assets/GameNeeds/enemyPumpkin/New Folder/script/Enemy.cs
@@ -4,33 +4,48 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform tomato;
+    private Rigidbody enemyRb;
+    private Transform tomato;
     public float distans;
-    public float speed;
+    public float speed = 10.0f;
     public float howClose;
     public bool enemyAttack;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        tomato = GameObject.FindGameObjectWithTag("Tomato").transform;
+        enemyRb = GetComponent<Rigidbody>();
+        //tomato = GameObject.FindGameObjectWithTag("Tomato").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        distans = Vector3.Distance(tomato.position, transform.position);
-
-        if (distans <= howClose)
+        if (!tomato)
         {
-            transform.LookAt(tomato);
-            GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+            //tomato = GameObject.FindGameObjectWithTag("Tomato").transform;
+            var tomatos = GameObject.FindGameObjectsWithTag("Tomato");
+            if(tomatos.Length > 0)
+            {
+                distans = Vector3.Distance(tomatos[0].transform.position, transform.position);
+
+                transform.LookAt(tomatos[0].transform);
+                GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+
+            }
         }
+        //if (distans <= howClose)
+        //{
+        //    transform.LookAt(tomato);
+        //    GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+        //}
 
         if (distans <= 2.0f)
         {
 
         }
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -39,6 +54,7 @@ public class Enemy : MonoBehaviour
         {
             enemyAttack = true;
             Destroy(other.gameObject);
+            tomato = null;
         }
     }
 }
