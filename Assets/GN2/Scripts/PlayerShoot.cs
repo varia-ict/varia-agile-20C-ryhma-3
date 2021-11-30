1dbedf52c903430ec5a67707dcd6dc2bb0bde52c
Niamat2222 <niamat.akbari@outlook.com>
2021-11-30 13:59:05 +0200

    gunshooting

---
+++ Assets/GN2/Scripts/PlayerShoot.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject tomato;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+
+            _ = Instantiate(tomato, transform.position, transform.rotation);
+            tomato.transform.position = transform.position + transform.forward;
+        }
+    }
+}
--- Assets/GameNeeds/enemyPumpkin/New Folder/script/Tomato.cs
+++ Assets/GameNeeds/enemyPumpkin/New Folder/script/Tomato.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Tomato : MonoBehaviour
 {
+    public float speed;
     private Rigidbody tomatoRb;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,7 @@ public class Tomato : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        transform.Translate(Vector3.back * Time.deltaTime * speed);
     }
 }
