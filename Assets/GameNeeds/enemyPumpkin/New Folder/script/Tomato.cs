4d6bbf6a1953c9e3f79a9933658648fe54ae0442
Niamat2222 <niamat.akbari@outlook.com>
2021-12-02 14:51:38 +0200

    tomatocollition

--- Assets/GameNeeds/enemyPumpkin/New Folder/script/Tomato.cs
+++ Assets/GameNeeds/enemyPumpkin/New Folder/script/Tomato.cs
@@ -6,6 +6,7 @@ public class Tomato : MonoBehaviour
 {
     public float speed;
     private Rigidbody tomatoRb;
+    public float breakpoint = 48f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +18,16 @@ public class Tomato : MonoBehaviour
     {
 
         transform.Translate(Vector3.back * Time.deltaTime * speed);
+
+        if (transform.position.y > breakpoint)
+        {
+            Debug.Log(transform.position.y);
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collidoi " + other);
+        Destroy(gameObject);
     }
 }
