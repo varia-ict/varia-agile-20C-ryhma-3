06f259bccecfde08e88d31f5917e43ca72ec45d0
Samuli Kärkkäinen <samulikarkkainen@Ohjelmointi-14-MacBook-Pro.local>
2021-12-07 12:51:37 +0200

    enemyspawnia

--- Assets/SamulinScripts/EnemyWave.cs
+++ Assets/SamulinScripts/EnemyWave.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawn : MonoBehaviour
+public class EnemyWaves : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
