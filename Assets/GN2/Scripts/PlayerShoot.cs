feebb95650508e80a0bbba99d4606194f1d927c5
Sameerwaseem21 <sairwaseem2015@hotmail.com>
2021-12-10 09:56:18 +0200

    gundone

--- Assets/GN2/Scripts/PlayerShoot.cs
+++ Assets/GN2/Scripts/PlayerShoot.cs
@@ -8,28 +8,16 @@ public class PlayerShoot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        EnemyScript enemyScript;
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
 
             _ = Instantiate(tomato, transform.position, transform.rotation);
             tomato.transform.position = transform.position + transform.forward;
         }
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Enemy") 
-        {
-            Destroy(collision.gameObject);
-        }
-
     }
 }
