58bd7d1501946fbbe975cc235f45227fee878fc3
Samuli Kärkkäinen <samulikarkkainen@Ohjelmointi-14-MacBook-Pro.local>
2021-12-13 14:35:16 +0200

    korjauksiamaanantai

--- Assets/GN2/SamulinScripts/SamulinPlayerShoot.cs
+++ Assets/GN2/SamulinScripts/SamulinPlayerShoot.cs
@@ -15,8 +15,9 @@ public class SamulinPlayerShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) { //jos painat mouse 1 pelaaja ampuu
 
+            //instantiate ottaa sisäänsä ja katsoo tomaatinarvot, sen position sen rotaation ja suunnan mihin liikkuu
             Instantiate(tomato, transform.position, transform.rotation);
             tomato.transform.position = transform.position + transform.forward;
         }
